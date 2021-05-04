cfa24f9acd709560e755cc98936347cfd95a5bd5
Rob <samantha@MacBook-Air.local>
2021-05-04 16:26:03 +1200

    First commit

--- designs.js
+++ designs.js
@@ -1,4 +1,4 @@
-// Select color input
+// define the variables
 var color = document.getElementById('colorPicker');
 var grid = document.getElementById('pixelCanvas');
 var gridsize = document.getElementById('sizePicker');
@@ -9,6 +9,7 @@ var width = document.getElementById('inputWidth').value;
 function selection(event) {
 };
 
+//set grid size according to input
 sizePicker.addEventListener ('click', function (event1) {
   selection(event1);
   event1.preventDefault();
@@ -18,14 +19,13 @@ sizePicker.addEventListener ('click', function (event1) {
   makeGrid(height, width);
 });
 
-
+//make the grid
 function makeGrid(height, width) {
   for (var r = 0; r < height; r++) {
     var row = grid.insertRow(r);
-//    console.log(row)
     for (var c = 0; c < width; c++) {
       var cell = row.insertCell(c);
-//      console.log(cell)
+//set the colour according to selection
       cell.addEventListener('click', function (event2) {
         event2.target.style.background = colorPicker.value;
       });
